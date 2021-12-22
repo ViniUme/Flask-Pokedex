295866f065b0f8efddf75e24bd3dacce0876fbf2
Vinícius Umetsu <66230638+ViniUme@users.noreply.github.com>
2021-12-22 16:21:03 -0300

    test list of pokemons

--- pokedex.py
+++ pokedex.py
@@ -1,10 +1,36 @@
-from  flask import Flask, render_template
+from os import name
+from flask import Flask, render_template
+import requests
+import json
+
+class Pokemon:
+    def __init__(self,name,type,img):
+        self.name = name
+        self.type = type
+        self.img = img
+
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template("index.html")
+    list_pokemons = []
+    id_pokemon = 1
+    
+    try:
+        while (id_pokemon != 10):
+            url = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}").text)
+            name = url['forms'][0]['name']
+            type = url['types'][0]['type']['name']
+            img = url['sprites']['front_default']
+            list_pokemons.append(Pokemon(name, type,img))
+            id_pokemon = id_pokemon + 1
+    except:
+        return "request erro"
+    
+    return render_template("index.html",
+    list_pokemons = list_pokemons
+    )
 
 @app.route("/pokemon/<idPokemon>")
 def pokemon(idPokemon):
