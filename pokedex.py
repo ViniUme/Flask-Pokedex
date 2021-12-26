aa0e58127cba063ecfb4c93a80701a50d367450e
Vinícius Umetsu <66230638+ViniUme@users.noreply.github.com>
2021-12-26 15:29:20 -0300

    limit

--- pokedex.py
+++ pokedex.py
@@ -22,8 +22,9 @@ def index():
     list_pokemons = []
     id_pokemon = 1
     
-    try:
-        while (id_pokemon != 29):
+    
+    while (id_pokemon != 29):
+        try:
             url = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}").text)
             name = url['forms'][0]['name']
             id = id_pokemon
@@ -38,16 +39,12 @@ def index():
             img = url['sprites']['front_default']
             list_pokemons.append(Pokemon(id,name,type1,type2,img))
             id_pokemon = id_pokemon + 1
-    except:
-        return "request erro"
+        except:
+            return "request erro"
     
     return render_template("index.html",
     list_pokemons = list_pokemons
     )
-
-@app.route("/pokemon/<idPokemon>")
-def pokemon(idPokemon):
-    return render_template("pokemon.html", idPokemon=idPokemon)
 
 if (__name__ == "__main__"):
     app.run(debug=True)
