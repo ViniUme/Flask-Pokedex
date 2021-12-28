75651655864eb8445e5f98816912975b51c577ea
Vinícius Henrique dos Santos <66230638+ViniUme@users.noreply.github.com>
2021-12-28 11:49:05 -0300

    up used files

--- pokedex.py
+++ pokedex.py
@@ -18,12 +18,14 @@ class Pokemon:
 app = Flask(__name__)
 
 @app.route("/")
-def index():
+@app.route("/home")
+def home():
+    
     list_pokemons = []
+    const_id = 26
     id_pokemon = 1
     
-    
-    while (id_pokemon != 29):
+    while (id_pokemon != const_id):
         try:
             url = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}").text)
             name = url['forms'][0]['name']
@@ -42,9 +44,71 @@ def index():
         except:
             return "request erro"
     
-    return render_template("index.html",
-    list_pokemons = list_pokemons
+    return render_template("home-pokepage.html",
+    list_pokemons = list_pokemons,
+    id_pokepage = 1
     )
+
+@app.route("/home/pokepage<id_page>")
+def pokepage(id_page):
+    
+    list_pokemons = []
+    
+    if (int(id_page) == 36):
+        
+        const_id = 899
+        id_pokemon = 876
+        
+        while (id_pokemon != const_id):
+            try:
+                url = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}").text)
+                name = url['forms'][0]['name']
+                id = id_pokemon
+
+                if (len(url['types']) == 2):
+                    type1 = url['types'][0]['type']['name']
+                    type2 = url['types'][1]['type']['name']
+                else:
+                    type1 = url['types'][0]['type']['name']
+                    type2 = None
+
+                img = url['sprites']['front_default']
+                list_pokemons.append(Pokemon(id,name,type1,type2,img))
+                id_pokemon = id_pokemon + 1
+            except:
+                return "request erro"
+        
+        return render_template("home-pokepage.html",
+        list_pokemons = list_pokemons
+        )
+        
+    else:
+        
+        const_id = int(id_page) * 25 + 1
+        id_pokemon = const_id - 25
+        
+        while (id_pokemon != const_id):
+            try:
+                url = json.loads(requests.get(f"https://pokeapi.co/api/v2/pokemon/{id_pokemon}").text)
+                name = url['forms'][0]['name']
+                id = id_pokemon
+
+                if (len(url['types']) == 2):
+                    type1 = url['types'][0]['type']['name']
+                    type2 = url['types'][1]['type']['name']
+                else:
+                    type1 = url['types'][0]['type']['name']
+                    type2 = None
+
+                img = url['sprites']['front_default']
+                list_pokemons.append(Pokemon(id,name,type1,type2,img))
+                id_pokemon = id_pokemon + 1
+            except:
+                return "request erro"
+        
+        return render_template("home-pokepage.html",
+        list_pokemons = list_pokemons
+        )
 
 if (__name__ == "__main__"):
     app.run(debug=True)
